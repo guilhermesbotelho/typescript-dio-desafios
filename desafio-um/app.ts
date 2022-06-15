4e9018bcfc4429b6fd43ce78ffc25e23f01fe026
Guilherme Siqueira Botelho <guilhermesbotelho@outlook.com>
2022-06-15 15:52:13 -0300

    desafio um resolvido

---
+++ desafio-um/app.ts
@@ -0,0 +1,11 @@
+// Como podemos rodar isso em um arquivo .ts sem causar erros? 
+
+const employee = {
+    code: 10,
+    name: "John",
+}
+
+const employeeUm: {code: number, name: string} = {
+    code: 10,
+    name: "John",
+}
